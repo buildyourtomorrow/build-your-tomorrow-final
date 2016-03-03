a3e01aeb23f9b7eb89ab74e32657a000889dc9ed
Eliezer Nunez <nunezeliezer@gmail.com>
2016-03-02 20:38:34 -0500

    Fix Period Start Date in dashboard

--- public/javascripts/factories/monthlyExpensesFactory.js
+++ public/javascripts/factories/monthlyExpensesFactory.js
@@ -53,6 +53,7 @@ app.factory('monthlyExpensesFactory', ['$http', 'incomeFactory', 'auth', '$state
 		var x1 = new Date(year, month, 1);
 		o.periodStart = [];
 		o.periodStart.push(x1);
+		o.periodStart.push(1);
 	};
 	o.calcPeriodEnd = function(){
 		var today = new Date();
