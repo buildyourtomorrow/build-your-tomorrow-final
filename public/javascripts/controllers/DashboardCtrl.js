ea82347756a65903b5892771bdd8fc215330104a
Eliezer Nunez <nunezeliezer@gmail.com>
2016-03-02 20:11:03 -0500

    Fix Period Start Date in dashboard

--- public/javascripts/controllers/DashboardCtrl.js
+++ public/javascripts/controllers/DashboardCtrl.js
@@ -10,7 +10,6 @@ app.controller('DashboardCtrl', ['$scope', 'monthlyExpensesFactory', 'auth', fun
 	$scope.dailyBudget = monthlyExpensesFactory.dailyBudget;
 	$scope.daysLeft = monthlyExpensesFactory.daysLeft;
 	$scope.periodStart = monthlyExpensesFactory.periodStart[0];
-	console.log(monthlyExpensesFactory.periodStart)
 	$scope.periodEnd = monthlyExpensesFactory.periodEnd[0];
 	$scope.today = monthlyExpensesFactory.today[0];
 
