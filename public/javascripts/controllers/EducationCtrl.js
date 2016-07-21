ef35f792f547eeb1798301f4b14ffd0b64f96831
Eliezer Nunez <nunezeliezer@gmail.com>
2016-07-21 16:29:15 -0400

    Add auth0 bug fix 8

--- public/javascripts/controllers/EducationCtrl.js
+++ public/javascripts/controllers/EducationCtrl.js
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
 app.controller('EducationCtrl', ['$scope',
 								 'store',
 								 'auth',
 								 'educationFactory',						
-								 '$state', function($scope, 
-								 				    store,
-								 				    auth,
-								 				    educationFactory,
-								 				    $state){
+								 '$state', function($scope, store, auth, educationFactory, $state){
 
-=======
-app.controller('EducationCtrl', ['$scope', '$state', 'auth', function($scope, $state, auth){
-
-  $scope.isLoggedIn = auth.isLoggedIn;
-
-  	
-  };
->>>>>>> 28c6f978a4ba08bdae3bd531dac6bebf07cd4f5d
 }])
